277d7b264632d2c216afedf93bc519848e50c936
Cristina Ferrer <krishee107@gmail.com>
2023-04-23 11:39:09 +0200

    Estilos para las pantallas y opciones

--- src/navigator/StackNavigator.tsx
+++ src/navigator/StackNavigator.tsx
@@ -7,10 +7,22 @@ export const StackNavigator = () => {
     const Stack = createStackNavigator();
 
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="Pagina1Screen" component={Pagina1Screen} />
-            <Stack.Screen name="Pagina2Screen" component={Pagina2Screen} />
-            <Stack.Screen name="Pagina3Screen" component={Pagina3Screen} />
+        <Stack.Navigator
+            // initialRouteName='Pagina2Screen' --> Definir página inicial
+            screenOptions={{
+                //headerShown: false, --> oculta el header del menú
+                headerStyle: {
+                    elevation: 0, //Le quita el '3d' a la app y hace desaparecer la sombra
+                    shadowColor: 'transparent' //iOS no respeta la propiedad anterior y se tiene que poner como transparente
+                },
+                cardStyle: {
+                    backgroundColor: 'white' //Color de fondo de la app
+                }
+            }}
+        >
+            <Stack.Screen name="Pagina1Screen" options={{ title: "Página 1" }} component={Pagina1Screen} />
+            <Stack.Screen name="Pagina2Screen" options={{ title: "Página 2" }} component={Pagina2Screen} />
+            <Stack.Screen name="Pagina3Screen" options={{ title: "Página 3" }} component={Pagina3Screen} />
         </Stack.Navigator>
     );
 }
--- src/screens/Pagina2Screen.tsx
+++ src/screens/Pagina2Screen.tsx
@@ -1,13 +1,21 @@
 import { Button, Text, View } from 'react-native'
 import { useNavigation } from '@react-navigation/core'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { styles } from '../theme/appTheme';
 
 export const Pagina2Screen = () => {
     const navigator = useNavigation();
+    useEffect(() => {
+        navigator.setOptions({
+            //title: 'Titulo de la pagina' --> Sobreescribe el titulo enviado en nuestro stacknavigator
+            headerBackTitle: 'Atrás' // --> Texto del botón "volver" en iOS, si se deja vació sale volver/back según el idioma
+        })
+    }, [])
+
 
     return (
         <View>
-            <Text>Pagina2Screen</Text>
+            <Text style={styles.title}>Pagina2Screen</Text>
 
             <Button
                 title="Ir a la página 3"
