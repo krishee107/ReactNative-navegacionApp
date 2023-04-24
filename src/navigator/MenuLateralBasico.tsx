4379b115e5faac92f34684e1c7f7b3ae1a7345e2
krishee107 <krishee107@gmail.com>
2023-04-24 22:37:13 +0200

    Menu lateral al tumbar el movil

--- src/navigator/MenuLateralBasico.tsx
+++ src/navigator/MenuLateralBasico.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StackNavigator } from './StackNavigator';
 import { SettingsScreen } from '../screens/SettingsScreen';
+import { useWindowDimensions } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export const MenuLateralBasico = () => {
+
+    const dimensions = useWindowDimensions();
+
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        drawerType: dimensions.width >= 768 ? 'permanent' : 'front',
+      }}>
       <Drawer.Screen name="StackNavigator" component={StackNavigator} />
       <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
     </Drawer.Navigator>
