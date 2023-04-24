d9f70360b3f70817bd880a996a842061d5ea2cd4
krishee107 <krishee107@gmail.com>
2023-04-24 22:23:55 +0200

    Añadir reanimated

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigator/StackNavigator';
+import { MenuLateralBasico } from './src/navigator/MenuLateralBasico';
 
 export default function App() {
   return (
     <NavigationContainer>
 
-      <StackNavigator />
+      {/*<StackNavigator />*/}
+      <MenuLateralBasico/>
 
     </NavigationContainer>
   );
