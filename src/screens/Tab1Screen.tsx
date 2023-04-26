7cab2948368aebd12825bda74517c725c33d6ce0
krishee107 <krishee107@gmail.com>
2023-04-26 21:26:50 +0200

    Configuración de los vector icons para Android

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { styles } from '../theme/appTheme'
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 export const Tab1Screen = () => {
     return (
-        <View>
-            <Text>Tab1Screen</Text>
+        <View style={styles.globalMargin}>
+            <Text style={styles.title}> Iconos </Text>
+            <Icon name="airplane-outline" size={80} color="#900" />
+            <Icon name="attach-outline" size={80} color="#900" />
+            <Icon name="bonfire-outline" size={80} color="#900" />
+            <Icon name="calculator-outline" size={80} color="#900" />
+            <Icon name="chatbubble-ellipses-outline" size={80} color="#900" />
         </View>
     )
 }
