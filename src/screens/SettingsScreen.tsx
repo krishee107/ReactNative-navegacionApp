2aa81dac300c1d99512396ee5c675ed9f372caa1
krishee107 <krishee107@gmail.com>
2023-04-27 13:13:37 +0200

    Mostrar la información del Context en la pantalla de Settings

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Text, View } from 'react-native'
+import { styles } from '../theme/appTheme'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { AuthContext } from '../context/AuthContext'
 
 export const SettingsScreen = () => {
+  const insets = useSafeAreaInsets();
+
+  const { authState } = useContext(AuthContext);
+
+
   return (
-    <View>
-        <Text>Settings screen</Text>
+    <View style={{
+      ...styles.globalMargin,
+      marginTop: insets.top + 20
+    }}>
+      <Text style={styles.title}>Settings screen</Text>
+      <Text>{JSON.stringify(authState, null, 4)} </Text>
     </View>
   )
 }
